523e5118703c97e931ea37ee00408ffcc7ed9bf1
Егор <egorhacker05@gmail.com>
2025-03-21 13:02:47 +1000

    creating devices.py

---
+++ source/devices.py
@@ -0,0 +1,41 @@
+from typing import Callable
+from pyttsx3 import init, Engine
+
+from requests import get
+
+class MessageInfo:
+    def __init__(self, text: str, attributes: list[dict[str, object]]) -> None:
+        self.text = text
+        self.attributes = attributes
+
+    text: str = ""
+    attributes: list[dict[str, object]] = [ ]
+
+    def __str__(self) -> str: return self.text
+
+class Speaker:
+    _driver: Engine = init()
+
+    def setupDriverAttributes(self, attributes: list[dict[str, object]]) -> None: 
+        for attribute in attributes:
+            self._driver.setProperty(attribute.keys(), attribute.values())
+        
+        print("current attributes: " + attributes)
+
+    def speak(self, message: MessageInfo) -> None:
+        self.setupDriverAttributes(message.attributes)
+
+        self._driver.say(message)
+        self._driver.runAndWait()
+
+    def __init__(self, language: str) -> None: self.setupDriverAttributes([{"voice", language}])
+
+class SpeechRecognizer:
+    _recognitionMethod: Callable = None
+
+    def recordAndRecognizeSpeech(self) -> MessageInfo:
+        return self._recognitionMethod()
+    
+    def _checkInternetConnection(self) -> bool:
+
+
