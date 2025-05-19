5f2a7d187353b4e772691235927bb57567b1d1eb
EgorHivrich <egorhacker05@gmail.com>
2025-05-19 07:45:45 +0100

    project root directory cleaning

--- source/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import json
-
-class FileLogger:
-    _messages: list[str] = []
-    
-    def __init__(self, filePath: str = "./logs.txt") -> None:
-        self._filePath = filePath
