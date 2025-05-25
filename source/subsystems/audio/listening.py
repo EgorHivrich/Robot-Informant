2ca20382240ae22235886e6e15cb86d16dffe4fe
EgorHivrich <egorhacker05@gmail.com>
2025-05-25 03:27:42 +0100

    fixed listening function

--- source/subsystems/audio/listening.py
+++ source/subsystems/audio/listening.py
@@ -1,18 +1,20 @@
+import sys
+sys.path.insert(1, "home/admin/Desktop/RobotInformant/source")
+
+print(sys.path)
+
 from speech_recognition import (
 	Microphone, Recognizer
 ) 
-import sys
-sys.path.insert(1, "/home/admin/Robot-Informant/source")
-
 from utility.connection import isThereInternetConnection
 
 def listenAndRecognizeSpeech() -> str:
-	with Microphone() as microphone:
-		recognizer = Recognizer()
-		text = recognizer.listen(microphone)
-	try:
-		return recognizer.recognize_google(text)
-	except Exception as error: return error
+    try:
+	    with Microphone() as microphone:
+            recognizer = Recognizer()
+            return recognizer.recognize_google(recognizer.listen(microphone))
+	
+    except Exception as error: return error
 
 if isThereInternetConnection().isAvailable == True:
 	print("connection is available now")
